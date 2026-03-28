1a08f6ceb4f4388142c02121d164991bd371864f
ANKITA09-ART <ankiankita903@gmail.com>
2026-03-28 10:56:04 +0530

    Add files via upload

---
+++ 1st project.py
@@ -0,0 +1,79 @@
+#print(hello world)
+#print(type(a))
+#hi i am ankita
+#arithemetic operators
+a=10
+b=2
+
+a=14
+b=7
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a**b)
+
+#relational operators
+a=10
+b=9
+print(a==b)
+print(a!=b)
+print(a>=b)
+print(a>b)
+print(a<=b)
+print(a<b)
+
+# assignment operators
+num=12
+num-=12
+print("num:",num)
+
+#logical operators
+a=50
+b=30
+print(not False)
+print(not(a>b))
+
+val1=True
+val2=True
+print("And operator:",val1 and val2)
+print("OR operator:",(a==b)or(a>b))
+
+#type conversion
+a='10'
+b=6.6
+#print(a+b)
+
+a=10
+b=78.5
+print(a-b)
+
+
+a=int("100")
+b=150
+
+print(a+b)
+
+#name=input("enter you name:")
+#print("you entered",name)
+#int("5")
+#val= float(input("enter some value:"))
+#print(type(val,val))
+
+#name=input("enter name:")
+#age=int(input("enter age:"))
+#marks= float(input("enter marks:"))
+#print("welcome",name)
+#print("age=",age)
+#print("marks=",name)
+
+a= float(input("enter first:"))
+b= float(input("enter second:"))
+
+print(a>=b)
+
+
+
+
+
---
+++ 2program.py
@@ -0,0 +1,38 @@
+#coneatenation
+str1="i am "
+str2="m ankita"
+final_str= str1+str2
+print(final_str)
+
+str1= "i"
+str2="love"
+str3="you"
+str4="tilak"
+final_str=str1+str2+str3+str4
+print(final_str)
+
+str1="i am ankita.\n i am studying in class 11th."
+print(str1)
+
+str1="tilak is a fool.\n he is big buffalo"
+print(str1)
+
+
+str1="SRM"
+len1=len(str1)
+print(len1)
+str2="institute"
+len2=len(str2)
+print(len2)
+final_str=str1+str2
+print(len(final_str))
+
+#index 
+str="odisha adharsha vidyalya"
+ch=str[1]
+print(ch)
+
+
+
+
+
